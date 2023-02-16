7e9b5d7a7c3b39931632d3c4c9783a58b30110de
kikiaziss123 <kameliarizki76@gmail.com>
2023-02-16 22:54:26 +0900

    first commit

---
+++ content/home.php
@@ -0,0 +1,5 @@
+<div class="card">
+    <h2>Home</h2>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque tenetur, eaque natus quam rerum harum dolorum! Obcaecati tempora consequatur corrupti odit
+     neque esse aliquam facilis asperiores placeat quam, soluta cum. AHMAD
+</div>
---
+++ public/content/home.php
@@ -0,0 +1,11 @@
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="alert alert-info">
+                   <center>Selamat Datang</center> 
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
